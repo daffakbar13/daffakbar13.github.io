d2ee048974fff0555e77577925e025cd7e88577d
M Daffa Akbar <m.daffa.akbar@wgs-id.com>
2022-12-28 18:34:22 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const repo = 'daffakbar13.github.io'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
 const nextConfig = {
+  assetPrefix,
+  basePath,
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: 'imgix',
+    path: `${repo}/`,
+  },
 }
 
 module.exports = nextConfig
